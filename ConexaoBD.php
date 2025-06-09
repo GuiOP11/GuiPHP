a89c78331c4aebc5b618a122c9a48a92295dd52c
GuiOP11 <deoliveirapadilhag@gmail.com>
2025-06-09 16:59:54 -0300

    eufiz

--- ConexaoBD.php
+++ ConexaoBD.php
@@ -12,3 +12,8 @@ class ConexaoBD{
 // APÓS ESSA LINHA, TUDO SERÁ APAGADO
 
 $conexao = ConexaoBD::conectar();
+
+//$sql = "insert into carro (modelo, marca, ano) values ('Strada Vermelha','Fiat','2013')";
+$sql = "delete from carro  where idcarro=2 ";
+
+$conexao->exec($sql);
