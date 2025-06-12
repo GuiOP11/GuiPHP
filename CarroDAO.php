7aa300ab4ec5d9b28994fb7cc2901f5b122cebf0
GuiOP11 <deoliveirapadilhag@gmail.com>
2025-06-12 17:01:51 -0300

    opa

--- CarroDAO.php
+++ CarroDAO.php
@@ -1,9 +1,9 @@
 <?php
-
+require_once "ConexaoBD.php";
 class CarroDAO{
     static function cadastrar($dados) {
         $conexao = ConexaoBD :: conectar();
-        $sql = 'insert into carro (modelo,marca, ono) value (?,?,?)';
+        $sql = 'insert into carro (modelo,marca, ano) values (?,?,?)';
         $stmt = $conexao-> prepare($sql);
         $stmt-> bindParam(1, $dados['modelo']);
           $stmt-> bindParam(2, $dados['marca']);
---
+++ cadastro-carro.php
@@ -0,0 +1,6 @@
+<?php
+require_once "CarroDAO.php";
+CarroDAO:: cadastrar($_GET);
+
+
+echo "<h3>Deu Opa 12 06";
